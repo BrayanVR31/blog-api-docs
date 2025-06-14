dedffee32acb19bcb81b0d9f71b115c5c607f4ac
Brayan Vázquez Ramírez <brayan.vazquez.ramirez.2000@gmail.com>
2025-06-13 20:19:03 -0600

    Add basic project structure blog api docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import starlight from "@astrojs/starlight";
+import mdx from "@astrojs/mdx";
+import { astroExpressiveCode } from '@astrojs/starlight/expressive-code';
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [icon(), astroExpressiveCode(), mdx(), starlight({
+    title: "API Blog System",
+    customCss: [
+      "./src/styles/globals.css"
+    ]
+  })],
+  markdown: {
+    shikiConfig: {
+      theme: "tokyo-night"
+    }
+  }
+});
---
+++ src/content/docs/index.md
@@ -0,0 +1,6 @@
+---
+title: Intro to blog API
+description: Learn more about me
+---
+
+#Welcome my api docs!
